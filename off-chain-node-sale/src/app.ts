d503bb054e32d73ab655706762668654651b24a1
Shivam Agarwal <shivam@synesxi.com>
2024-07-15 13:43:21 +0530

    Final commit with UI

--- off-chain-node-sale/src/app.ts
+++ off-chain-node-sale/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import onRequestReceived from "./middlewares/onRequestReceived"
+import onRequestReceived, { ICustomRequest } from "./middlewares/onRequestReceived"
 import { dataSource } from './data-source';
 import { NFTOwner } from './entities/NFTOwner';
 import { FractionalNFT } from './entities/FractionalNFT';
@@ -11,7 +11,7 @@ const app = express()
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://192.168.1.119:5173'],
     methods: ['POST'],
-    allowedHeaders: ['Content-Type', 'x-t-id']
+    allowedHeaders: ['Content-Type', 'address']
 };
 
 app.use(cors(corsOptions))
@@ -24,48 +24,81 @@ const fractionalNFTRepository = dataSource.getRepository(FractionalNFT);
 const batchRepository = dataSource.getRepository(NFTBatch);
 
 app.post('/purchase', async (req, res) => {
-    const { walletAddress, fractionalNFTContractAddress, fractionalNFTTokenID, chain, chainID } = req.body;
+    const dbTransaction = dataSource.createQueryRunner()
+    try {
+        const { walletAddress, chain, chainId, quantity } = req.body;
 
-    let user = await userRepository.findOne({ where: { walletAddress } });
-    if (!user) {
-        user = new NFTOwner();
-        user.walletAddress = walletAddress;
-        await userRepository.save(user);
+        const contract = {
+            fractionalNFTContractAddress: 'fractionalNFTContractAddress',
+            fractionalNFTTokenID: 'fractionalNFTTokenID',
+        }
+
+        const fractions = await fractionalNFTRepository.findOne({ where: { owner: { walletAddress } } });
+        // if (fractions) return res.status(401).send({ message: "You have some fractions already." })
+        if (!walletAddress) return res.status(401).send({ message: "Your address is invalid" })
+
+        await dbTransaction.startTransaction()
+        let user = await userRepository.findOne({ where: { walletAddress } });
+        if (!user) {
+            user = new NFTOwner();
+            user.walletAddress = walletAddress;
+            await dbTransaction.manager.save(user);
+        }
+
+        const fractionalNFT = new FractionalNFT();
+        fractionalNFT.owner = user;
+        fractionalNFT.fractionalNFTContractAddress = contract.fractionalNFTContractAddress;
+        fractionalNFT.fractionalNFTTokenID = contract.fractionalNFTTokenID;
+        fractionalNFT.chain = chain;
+        fractionalNFT.chainID = chainId;
+        fractionalNFT.quantity = quantity
+
+        let batch = await batchRepository.findOne({ where: { isFilled: false }, relations: ['fractionalNFTs'] });
+        if (!batch) {
+            batch = new NFTBatch();
+            batch.fractionalNFTs = [];
+            batch = await dbTransaction.manager.save(batch);
+            batch.nodeLicenseContractAddress = '0xNodeLicenseContractAddress';
+            batch.nodeLicenseTokenID = 'NodeLicenseTokenID';
+            batch.chain = chain;
+        }
+
+        fractionalNFT.batch = batch;
+        batch.fractionalNFTs.push(fractionalNFT);
+
+        if (batch.fractionalNFTs.length === 10) {
+            batch.isFilled = true;
+        }
+
+        await dbTransaction.manager.save(fractionalNFT);
+        batch = await dbTransaction.manager.save(batch);
+
+        await dbTransaction.commitTransaction()
+        res.send({ message: "Succesful" });
+    } catch (error) {
+        await dbTransaction.rollbackTransaction()
+        console.log({ error })
+        res.status(500).json(error)
     }
-
-    const fractionalNFT = new FractionalNFT();
-    fractionalNFT.owner = user;
-    fractionalNFT.fractionalNFTContractAddress = fractionalNFTContractAddress;
-    fractionalNFT.fractionalNFTTokenID = fractionalNFTTokenID;
-    fractionalNFT.chain = chain;
-    fractionalNFT.chainID = chainID;
-
-    let batch = await batchRepository.findOne({ where: { isFilled: false }, relations: ['fractionalNFTs'] });
-    if (!batch) {
-        batch = new NFTBatch();
-        batch.fractionalNFTs = [];
+    finally {
+        await dbTransaction.release()
     }
-
-    fractionalNFT.batch = batch;
-    batch.fractionalNFTs.push(fractionalNFT);
-
-    if (batch.fractionalNFTs.length === 10) {
-        batch.isFilled = true;
-        batch.nodeLicenseContractAddress = '0xNodeLicenseContractAddress';
-        batch.nodeLicenseTokenID = 'NodeLicenseTokenID';
-        batch.chain = chain;
-    }
-
-    await fractionalNFTRepository.save(fractionalNFT);
-    await batchRepository.save(batch);
-
-    res.send(batch);
 });
 
 app.get('/batches', async (req, res) => {
     const batches = await batchRepository.find({ relations: ['fractionalNFTs', 'fractionalNFTs.owner'] });
     res.send(batches);
 });
+
+app.get('/fractions', async (req: ICustomRequest, res) => {
+    const { user: { walletAddress } } = req
+    const fractions = await fractionalNFTRepository.find({ where: { owner: { walletAddress } } });
+    res.send(fractions);
+});
+
+app.post('/mint', () => {
+
+})
 
 app.post('/batch/fill', async (req, res) => {
     const batch = await batchRepository.findOne({ where: { id: req.body.batchId, }, relations: ['fractionalNFTs'] });
